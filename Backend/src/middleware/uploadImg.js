30ffba002ab3b63a28e228dc98db01b1c7d30ca3
IchsanR <111603307+IchsanR@users.noreply.github.com>
2022-10-22 17:05:04 +0800

    Jangan Update ke master

--- Backend/src/middleware/uploadImg.js
+++ Backend/src/middleware/uploadImg.js
@@ -1,95 +1,115 @@
-const multer = require('multer');
-const path = require('path');
-
-const name = path.basename(file.originalname);
-const ext = path.extname(file.originalname);
-const nameSplit = name.split(`${ext}`);
+const multer = require("multer");
+const path = require("path");
 
 const currentDate = new Date();
-const dateTime = currentDate.getDate() + (currentDate.getMonth()+1) + currentDate.getFullYear();
-const timestamp = currentDate.getHours() + currentDate.getMinutes() + currentDate.getSeconds() + currentDate.getMilliseconds();
+const dateTime =
+	currentDate.getDate() +
+	(currentDate.getMonth() + 1) +
+	currentDate.getFullYear();
+const timestamp =
+	currentDate.getHours() +
+	currentDate.getMinutes() +
+	currentDate.getSeconds() +
+	currentDate.getMilliseconds();
 
 const multerProfileUpload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, './assets/profile_pic');
-        },
-        filename: (req, file, cb) => {
-            const fileName = nameSplit[0] + ' - ' + dateTime + timestamp + ext;
-            cb(null, fileName);
-        }
-    }),
-    limits: {
-        // limit filesize up to 1mb
-        fileSize: 1024 * 1024,
-    },
+	storage: multer.diskStorage({
+		destination: (req, file, cb) => {
+			cb(null, "./assets/profile_pic");
+		},
+		filename: (req, file, cb) => {
+			const name = path.basename(file.originalname);
+			const ext = path.extname(file.originalname);
+			const nameSplit = name.split(`${ext}`);
+			const fileName = nameSplit[0] + " - " + dateTime + timestamp + ext;
+			cb(null, fileName);
+		},
+	}),
+	limits: {
+		// limit filesize up to 1mb
+		fileSize: 1024 * 1024,
+	},
 
-    fileFilter: (req, file, cb) => {
-        if(ext === '.jpg' || ext === '.png' || ext === '.jpeg' || ext === '.jfif'){
-            cb(null, true);
-        }else{
-            const error = {
-                message: 'File type is not supported'
-            }
-            cb(error, false);
-        }
-    }
-})
+	fileFilter: (req, file, cb) => {
+		const ext = path.extname(file.originalname);
+		if (
+			ext === ".jpg" ||
+			ext === ".png" ||
+			ext === ".jpeg" ||
+			ext === ".jfif"
+		) {
+			cb(null, true);
+		} else {
+			const error = {
+				message: "File type is not supported",
+			};
+			cb(error, false);
+		}
+	},
+});
 
 const multerAirlineUpload = multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, './assets/airline');
-        },
-        filename: (req, file, cb) => {
-            const fileName = nameSplit[0] + ' - ' + dateTime + timestamp + ext;
-            cb(null, fileName);
-        }
-    }),
-    limits: {
-        // limit filesize up to 1MB
-        fileSize: 1024 * 1024,
-    },
+	storage: multer.diskStorage({
+		destination: (req, file, cb) => {
+			cb(null, "./assets/airline");
+		},
+		filename: (req, file, cb) => {
+			const name = path.basename(file.originalname);
+			const ext = path.extname(file.originalname);
+			const nameSplit = name.split(`${ext}`);
+			const fileName = nameSplit[0] + " - " + dateTime + timestamp + ext;
+			cb(null, fileName);
+		},
+	}),
+	limits: {
+		// limit filesize up to 1MB
+		fileSize: 1024 * 1024,
+	},
 
-    fileFilter: (req, file, cb) => {
-        if(ext === '.jpg' || ext === '.png' || ext === '.jpeg' || ext === '.jfif' || ext === '.svg'){
-            cb(null, true);
-        }else{
-            const error = {
-                message: 'File type is not supported'
-            }
-            cb(error, false);
-        }
-    }
-})
+	fileFilter: (req, file, cb) => {
+		const ext = path.extname(file.originalname);
+		if (
+			ext === ".jpg" ||
+			ext === ".png" ||
+			ext === ".jpeg" ||
+			ext === ".jfif" ||
+			ext === ".svg"
+		) {
+			cb(null, true);
+		} else {
+			const error = {
+				message: "File type is not supported",
+			};
+			cb(error, false);
+		}
+	},
+});
 
 module.exports = {
-    uploadProfilePic: (req, res, next) => {
-        const multerSingle = multerProfileUpload.single('photo');
-        multerSingle(req, res, (err) => {
-            if(err){
-                res.json({
-                    message: 'upload failed',
-                    error: err
-                })
-            }else{
-
-                next();
-            }
-        })
-    },
-    uploadAirlineLogo: (req, res, next) => {
-        const multerSingle = multerAirlineUpload.single('logo');
-        multerSingle(req, res, (err) => {
-            if(err){
-                res.json({
-                    message: 'upload failed',
-                    error: err
-                })
-            }else{
-
-                next();
-            }
-        })
-    }
-}
+	uploadProfilePic: (req, res, next) => {
+		const multerSingle = multerProfileUpload.single("photo");
+		multerSingle(req, res, (err) => {
+			if (err) {
+				res.json({
+					message: "upload failed",
+					error: err,
+				});
+			} else {
+				next();
+			}
+		});
+	},
+	uploadAirlineLogo: (req, res, next) => {
+		const multerSingle = multerAirlineUpload.single("logo");
+		multerSingle(req, res, (err) => {
+			if (err) {
+				res.json({
+					message: "upload failed",
+					error: err,
+				});
+			} else {
+				next();
+			}
+		});
+	},
+};
