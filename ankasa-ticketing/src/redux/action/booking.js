ec9b571b98e306232941c373c287c108706ff4f1
Reza Fauzan Akbar <rezafauzanakbar3@gmail.com>
2022-10-26 18:28:30 +0700

    push booking detail

--- ankasa-ticketing/src/redux/action/booking.js
+++ ankasa-ticketing/src/redux/action/booking.js
@@ -1,1 +1,15 @@
 import axios from "axios";
+
+export const getDetailBooking = (id) => ({
+  type: "GET_DETAIL_BOOKING",
+  payload: new Promise((resolve, reject) => {
+    axios
+      .get(`http://localhost:3001/booking/detail/${id}`)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  }),
+});
